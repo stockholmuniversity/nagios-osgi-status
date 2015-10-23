bd9f4b7bd32f5f1f5f3d129cc3eadfbdf1ac7c50
Simon Lundström <simlu@su.se>
2015-10-23 13:31:09 +0200

    Add an example from route which works
    But is wrong since we want to use the built in jetty/pax-http.

--- src/main/groovy/se/su/it/servicemix/camel/osgi/StatusRoutes.groovy
+++ src/main/groovy/se/su/it/servicemix/camel/osgi/StatusRoutes.groovy
@@ -7,6 +7,9 @@ public class StatusRoutes extends RouteBuilder {
   public void configure() throws Exception {
 
     from("restlet:/status?restletMethods=GET,POST")
+    // This works, but starts another HTTP server...
+    // We want to use the already existing Jetty
+    // from("restlet:http://localhost:8080/status?restletMethods=GET,POST")
       .beanRef("Status", "status")
       .to("log:FAX");
 
